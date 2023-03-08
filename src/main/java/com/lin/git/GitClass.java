ea1182721623ad38dd106ac6b40666dd3f85a2ed
lin <lin@qq.com>
2023-03-08 17:50:45 +0800

    first commit

---
+++ src/main/java/com/lin/git/GitClass.java
@@ -0,0 +1,13 @@
+package com.lin.git;
+
+/**
+ * @Author :Lin
+ * @Description :
+ * @Date : Create in 17:47 2023/3/8
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+
+}
